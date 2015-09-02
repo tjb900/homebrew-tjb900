c646c67a2cc3bc416b46d760011db41a90a59da0
Tim Burgess <ozburgess@Tims-MacBook.local>
2015-09-02 14:34:31 -0300

    added udr 0.9.4

---
+++ Formula/udr.rb
@@ -0,0 +1,35 @@
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                http://www.rubydoc.info/github/Homebrew/homebrew/master/frames
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Udr < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/LabAdvComp/UDR/archive/v0.9.4.tar.gz"
+  version "0.9.4"
+  sha256 "fc3e42bd63b16ff8676dd973aac2cb9106777904440381a6fe41be007862da26"
+
+  # depends_on "cmake" => :build
+  depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    system "make", "-C", "udt/src", "-e", "os=OSX"
+    system "make", "-C", "src", "-e", "os=OSX"
+    system "mkdir", "#{bin}"
+    system "cp", "src/udr", "#{bin}"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test UDR`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "#{bin}/udr"
+  end
+end
